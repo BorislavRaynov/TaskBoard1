48cac0baa9f1b2ef50750fe4b332367ddaf15bf4
Borislav Raynov <b_raynov@abv.bg>
2023-10-08 10:50:02 +0300

    Deploy set up

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ resource "azurerm_linux_web_app" "alwa" {
   connection_string {
     name  = "DefaultConnection"
     type  = "SQLAzure"
-    value = "Data Source=tcp:${azurerm_mssql_server.sqls.name},1433;Initial Catalog=${azurerm_mssql_database.sqldb.name};User ID=${azurerm_mssql_server.sqls.administrator_login};Password=${azurerm_mssql_server.sqls.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqls.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sqldb.name};User ID=${azurerm_mssql_server.sqls.administrator_login};Password=${azurerm_mssql_server.sqls.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
   }
 }
 
